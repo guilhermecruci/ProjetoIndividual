23b0098a1c9b8ebb1e71cfce297c79763411074b
guilhermecruci <guilherme.cruci@sptech.school>
2023-11-24 16:03:14 -0300

    Adicionado o quiz

--- Site/public/js/index.js
+++
@@ -1,9 +0,0 @@
-const controls = document.querySelectorAll('.control');
-let currentItem = 0;
-const items = document.querySelectorAll('.item');
-const maxItems = items.length;
-controls.forEach(control => {
-    control.addEventListener('click', () => {
-        console.log('control clicked');
-    })
-});
---
+++ Site/public/js/quiz.js
@@ -0,0 +1,63 @@
+//Vetor de objetos
+var quiz = [
+
+    {
+        q: "Em que ano a Ordem Demolay foi criada?",
+        opcoes: ['1919', '1920', '1319', '1320'],
+        resposta: 0
+    },
+
+    {
+        q: "Quem foi seu criador?",
+        opcoes: ['Louis Gordon Lower', 'Geoffroy de Charney', 'Frank Shermann Land', 'Hugo de Payens'],
+        resposta: 2
+    },
+
+    {
+        q: "Em que lugar foi criada a ordem?",
+        opcoes: ['Londres', 'Texas', 'França', 'Grã-Bretanha'],
+        resposta: 1
+    },
+
+    {
+        q: 'Em que ano a Ordem Demolay veio para o Brasil?',
+        opcoes: ['1974', '2000', '1977', '1980'],
+        resposta: 3
+    },
+
+    {
+        q: 'Qual foi o primeiro capítulo no Brasil (Em qual estado)?',
+        opcoes: ['São Paulo', 'Rio de Janeiro', 'Brasília', 'Minas Gerais'],
+        resposta: 1
+    },
+
+    {
+        q: 'Quem foi que decretou o fim dos Cavaleiros templários?',
+        opcoes: ['Papa Clemente X', 'Rei Luís Filipe IV', 'Hugo de Payens', 'Napoleão Bonaparte'],
+        resposta: 1
+    },
+
+    {
+        q: 'Em 	que a Ordem Demolay foi inspirada?',
+        opcoes: ['Maçonaria', 'Ordem do Arco-Íris', 'Igreja', 'Cavaleiros Templários'],
+        resposta: 3
+    },
+
+    {
+        q: 'Quem foi o Louis Gordon Lower?',
+        opcoes: ['Um maçom', 'Um cavaleiro templário', 'Um ativista político ', 'O primeiro Demolay'],
+        resposta: 3
+    },
+
+    {
+        q: 'Quando foi realizado a prisão de Jacques Demolay?',
+        opcoes: ['11 de julho de 1236', '13 de outubro de 1307', '13 de agosto de 1344', '12 de setembro de 1300'],
+        resposta: 1
+    },
+
+    {
+        q: 'Qual o dia do Demolay no Brasil? ',
+        opcoes: ['15 de Abril', '12 de Agosto', '18 de Março', '25 de Novembro'],
+        resposta: 2
+    }
+]
